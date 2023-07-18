e8fa572014e71fda69a0c17ece5d748c48f93d62
mason3k <48703453+mason3k@users.noreply.github.com>
2023-07-18 16:07:34 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,5 @@
 $username = "TODO: ADD USERNAME"
-$ps_script_dir = "C:\Users\$username\Documents\Scripts"
+$ps_script_dir = "C:\Users\$username\Scripts"
 Import-Module posh-git
 Import-Module oh-my-posh
 oh-my-posh --init --shell pwsh --config "C:\Users\$username\scoop\apps\oh-my-posh\current\themes\negligible.omp.json" | Invoke-Expression
