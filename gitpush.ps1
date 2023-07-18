3d378b7fc4deeb8feb8c053f9b54d4ba21c263af
mason3k <48703453+mason3k@users.noreply.github.com>
2023-07-18 15:46:05 -0500

    Create gitpush.ps1

---
+++ gitpush.ps1
@@ -0,0 +1,26 @@
+$status = git status
+$status
+Get-Location
+$Continue = Read-Host -Prompt 'Does this look right?'
+if ($Continue -ne "y" -and $Continue -ne "Y")
+{
+	exit
+}
+$remotebranch = git status -sbz
+$remotebranch -match "## ([^. ]+)(?:\.\.\.([^ ]+))?"
+if ($Matches.count -gt 2)
+{
+	$remotebranch = $Matches[2]
+}
+else
+{
+	$remotebranch = "origin/" + $Matches[1]
+}
+git diff ...$remotebranch | dunk
+$Continue = Read-Host -Prompt 'Does this look right?'
+if ($Continue -ne "y" -and $Continue -ne "Y")
+{
+	exit
+}
+Write-Host "Pushing to remote..."
+git push origin HEAD
