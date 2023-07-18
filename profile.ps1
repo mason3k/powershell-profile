65501598dc6842a0fe46df7edf6345eb505021ea
mason3k <48703453+mason3k@users.noreply.github.com>
2023-07-18 16:15:17 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,7 @@
 $username = "TODO: ADD USERNAME"
 $ps_script_dir = "C:\Users\$username\Scripts"
 Import-Module posh-git
-Import-Module oh-my-posh
-oh-my-posh --init --shell pwsh --config "C:\Users\$username\scoop\apps\oh-my-posh\current\themes\negligible.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
 (Get-PSReadLineOption).HistoryNoDuplicates = $True
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionViewStyle ListView
