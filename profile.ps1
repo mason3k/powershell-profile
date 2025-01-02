49e2a917920e7cde4f0679195c2729eae4296790
mason3k <48703453+mason3k@users.noreply.github.com>
2025-01-02 11:12:55 -0600

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,9 @@
 $username = "TODO: ADD USERNAME"
 $ps_script_dir = "C:\Users\$username\Scripts"
+New-Alias push $ps_script_dir\gitpush.ps1
+New-Alias activate ./.venv/Scripts/Activate.ps1
 Import-Module posh-git
+Import-Module oh-my-posh
 oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
 (Get-PSReadLineOption).HistoryNoDuplicates = $True
 Set-PSReadLineOption -EditMode Emacs
